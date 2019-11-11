e5832eb5c23a950f375fe8f81c58dfeb1bdd72c6
Alejandra Izaguirre <aleizatebar@gmail.com>
2019-11-11 16:34:27 +0100

    Added layout with two sides

--- app/src/main/java/es/alejandra/findyourdream/activityListOfDreams.java
+++ app/src/main/java/es/alejandra/findyourdream/activityListOfDreams.java
@@ -13,11 +13,17 @@ import java.util.ArrayList;
 
 public class activityListOfDreams extends AppCompatActivity {
 
+    private boolean dosFragmentos;
+
     ArrayList<Contenido.Lista_entrada> lista = Contenido.ENT_LISTA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_dreams);
+
+        if(findViewById(R.id.frame_contenedor) != null){
+            dosFragmentos = true;
+        }
 
         final TextView author = findViewById(R.id.textViewAuthor);
         author.setOnClickListener(new View.OnClickListener() {
